7fd38df46b1ad965d1baf4cd11307173ffc9c924
Jiayun YU <jiayunyu@Jiayuns-MacBook-Pro-Black.local>
2018-08-05 00:50:19 -0400

    add tire object

--- cache.py
+++ cache.py
@@ -39,20 +39,7 @@ class cache_lru(object):
 		self.__print_order__()
 		return
 
-	def insert(self,key,value,order):
-		print ("Insert function with key:",key,"value:",value,"order:",order)
-		return
-
-	def search(self, value):
-		print ("Search function with value:",value)
-		return
-
-	def startwith(self,start):
-		print ("Start with function with start str:",start)
-		return
-
 	def __print_order__(self):
-		print (len(self.cache))
 		tmp_head = self.order_head
 		while tmp_head:
 			print (tmp_head.key,":",tmp_head.val)
@@ -86,12 +73,10 @@ class cache_lru(object):
 			self.order_head = tmp_head
 			tmp_head.pre = None
 		else: # already head 
-			print("Here")
 			return
 		return
 
 	def __remove_node__(self):
-		print ("current tail:",self.order_tail.key,self.order_tail.val)
 		new_tail = self.order_tail.pre
 		if new_tail:
 			new_tail.next = None
@@ -117,16 +102,20 @@ class cache_lfu(object):
 		print ("Put function with key:",key,"value:",value)
 		return
 
-	def insert(self,key,value,order):
-		print ("Insert function with key:",key,"value:",value,"order:",order)
+class tire(object):
+	def __init__(self):
+		super(tire,self).__init__()
+
+	def insert(self,key,value,index):
+		print("Insert function with key:",key,"value:",value,"index:",index)
 		return
 
-	def search(self, value):
-		print ("Search function with value:",value)
+	def search(self,key):
+		print("Search function with key:",key)
 		return
 
-	def startwith(self,start):
-		print ("Start with function with start str:",start)
+	def startwith(self,startstr):
+		print("Startwith function search startstr:",startstr)
 		return
 
 class flatten(object):
@@ -150,11 +139,8 @@ def lru_command():
 	example command:
 	get:1
 	put:1 2
-	insert:1 2 3
-	search:2
-	startwith:s
 	'''
-	var = input("Please enter command command:parameter0 parameter1 parameter2:\ne.g.[get|put|insert|search|startwith|exit]:[key|value|startstr] [value [order]]\n")
+	var = input("Please enter command command:parameter0 parameter1 parameter2:\ne.g.[get|put|exit]:[key] [value [order]]\n")
 	lru_cache = cache_lru(10)
 	while True:
 		if var == 'exit': break
@@ -162,9 +148,6 @@ def lru_command():
 		if len(varlist) > 1:
 			if varlist[0] == 'get' and len(varlist) == 2: lru_cache.get(varlist[1])
 			if varlist[0] == 'put' and len(varlist) == 2 and len(varlist[1].split()) == 2: lru_cache.put(varlist[1].split()[0],varlist[1].split()[1])
-			if varlist[0] == 'insert' and len(varlist) == 2 and len(varlist[1].split()) == 3: lru_cache.insert(varlist[1].split()[0],varlist[1].split()[1],varlist[1].split()[2])
-			if varlist[0] == 'search' and len(varlist) == 2: lru_cache.search(varlist[1])
-			if varlist[0] == 'startwith' and len(varlist) == 2: lru_cache.startwith(varlist[1])
 		else: print ("Invalid command, try again!")
 		var = input()
 
@@ -186,11 +169,8 @@ def lfu_command():
 	example command:
 	get:1
 	put:1 2
-	insert:1 2 3
-	search:2
-	startwith:s
 	'''
-	var = input("Please enter command command:parameter0 parameter1 parameter2:\ne.g.[get|put|insert|search|startwith|exit]:[key|value|startstr] [value [order]]\n")
+	var = input("Please enter command command:parameter0 parameter1 parameter2:\ne.g.[get|put|exit]:[key] [value [order]]\n")
 	lfu_cache = cache_lfu(2)
 	while True:
 		if var == 'exit': break
@@ -198,9 +178,24 @@ def lfu_command():
 		if len(varlist) > 1:
 			if varlist[0] == 'get' and len(varlist) == 2: lfu_cache.get(varlist[1])
 			if varlist[0] == 'put' and len(varlist) == 2 and len(varlist[1].split()) == 2: lfu_cache.put(varlist[1].split()[0],varlist[1].split()[1])
-			if varlist[0] == 'insert' and len(varlist) == 2 and len(varlist[1].split()) == 3: lfu_cache.insert(varlist[1].split()[0],varlist[1].split()[1],varlist[1].split()[2])
-			if varlist[0] == 'search' and len(varlist) == 2: lfu_cache.search(varlist[1])
-			if varlist[0] == 'startwith' and len(varlist) == 2: lfu_cache.startwith(varlist[1])
+		else: print ("Invalid command, try again!")
+		var = input()
+def tire_command():
+	'''
+	example command:
+	insert:1 2 3
+	search:2
+	startwith:s
+	'''
+	var = input("Please enter command command:parameter0 parameter1 parameter2:\ne.g.[insert|search|startwith|exit]:[key|startstr] [value [order]]\n")
+	t = tire()
+	while True:
+		if var == 'exit': break
+		varlist = var.split(':')
+		if len(varlist) > 1:
+			if varlist[0] == 'insert' and len(varlist) == 2 and len(varlist[1].split()) == 3: t.insert(varlist[1].split()[0],varlist[1].split()[1],varlist[1].split()[2])
+			if varlist[0] == 'search' and len(varlist) == 2: t.search(varlist[1])
+			if varlist[0] == 'startwith' and len(varlist) == 2: t.startwith(varlist[1])
 		else: print ("Invalid command, try again!")
 		var = input()
 
@@ -217,4 +212,8 @@ if __name__ == "__main__":
 	if mode == '3': 
 		print ("Flatter:")
 		flatten_command()
+	if mode == '4':
+		print ("Tire:")
+		tire_command()
+
 
